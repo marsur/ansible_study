cd4e3e35c6b728e619f3eba8bc2901c666f30db9
marsur <martin.suraba@gmail.com>
2022-07-19 09:44:44 +0200

    Create ping.sh

---
+++ scripts/ping.sh
@@ -0,0 +1,13 @@
+
+#!/bin/bash
+
+#ping IP 
+
+for i in {30..40}
+do 
+ping -c 3 192.168.191.$i 
+done 
+
+# Be careful...Fantomas see every your move!
+
+#Don't be stupid and belive: You'll never walk alone my red friend.
